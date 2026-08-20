4aabf3214137974fc7126de10b228871c3418f23
Kauan Brandão <brandaokauan277@gmail.com>
2026-08-20 17:25:34 -0300

    atv 1 corrigindo o erro

--- src/validator.ts
+++ src/validator.ts
@@ -1,6 +1,21 @@
+const TITULO_MIN_LENGTH = 5;
+const TITULO_MAX_LENGTH = 100;
+
 function validarChamado(titulo: string): boolean {
-    if (!titulo || titulo.trim() ==="") {
+    if (!titulo || titulo.trim() === "") {
         throw new Error("O título do chamado não pode ser vazio.");
+    }
+    
+    if (titulo.trim().length < TITULO_MIN_LENGTH) {
+        throw new Error(
+            `O título do chamado deve possuir no mínimo ${TITULO_MIN_LENGTH} caracteres.`
+        );
+    }
+
+    if (titulo.trim().length > TITULO_MAX_LENGTH) {
+        throw new Error(
+            `O título do chamado deve possuir no máximo ${TITULO_MAX_LENGTH} caracteres.`
+        );
     }
 
     return true;
